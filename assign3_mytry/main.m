9de60172387a3946c2f25c43a957eafeae05670b
fdesimini <fdesimini@users.noreply.github.com>
2015-04-07 12:54:58 -0400

    Initial Commit

---
+++ assign3_mytry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  assign3_mytry
+//
+//  Created by Frank Desimini on 2015-04-07.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
